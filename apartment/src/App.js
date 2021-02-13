f3e66b7cfb75da3f4a3a0c3f72a582e1a3cf3642
Ali <a.nategh76@gmail.com>
2021-02-13 16:50:47 +0330

    Prepared base project for team

---
+++ apartment/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from './components/Home/home'
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <Home />
+     );
+  }
+}
+ 
+export default App;
---
+++ apartment/src/components/Units/units.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Units extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+                <h1>واحد ها</h1>
+                <Table 
+                    items = {this.state.items}
+                    onClick = {(item) => this.handleClick(item)}
+                />
+            </>
+         );
+    }
+}
+ 
+export default Units;
