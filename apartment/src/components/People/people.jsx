66a7b2f7d1ee14e9641f8216e6a1f021bab43460
ferizoozoo <farhadzohoorz@gmail.com>
2021-02-15 06:44:42 +0330

    Create People table and add fake data to peopleServices

--- apartment/src/components/People/people.jsx
+++ apartment/src/components/People/people.jsx
@@ -1,12 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class People extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <h1>آدم ها</h1>
-         );
-    }
+  state = {
+      people: []
+  };
+
+  componentDidMount() {
+      
+  }
+
+  render() {
+    return (
+      <>
+        <h1>آدم ها</h1>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">نام</th>
+              <th scope="col">شماره تلفن</th>
+            </tr>
+          </thead>
+          <tbody>
+              {people.map((person, index) => (
+                  <tr>
+                  <th scope="row">{person.id}</th>
+                  <td>{person.name}</td>
+                  <td>{person.phone}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
- 
+
 export default People;
