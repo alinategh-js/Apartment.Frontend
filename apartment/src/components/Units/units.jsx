f349e588ffa1bbf26c54d2d4b4ef70f9b15cb863
reyhanekhorasani <r.khorasani.n@gmail.com>
2021-02-15 14:28:05 +0330

    Create table

--- apartment/src/components/Units/units.jsx
+++ apartment/src/components/Units/units.jsx
@@ -1,18 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import {getData} from "./unitsServices";
 
 class Units extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <>
-                <h1>واحد ها</h1>
-                {/* <Table 
-                    items = {this.state.items}
-                    onClick = {(item) => this.handleClick(item)}
-                /> */}
-            </>
-         );
-    }
+  state = {
+      units: [
+          
+      ]
+  };
+
+componentDidMount(){
+    this.setState({
+        units: getData()
+    })
 }
- 
+
+  render() {
+    return (
+      <>
+        <h1>واحد ها</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">شماره واحد</th>
+              <th scope="col">مالک</th>
+              <th scope="col">ساکن</th>
+              <th scope="col">متراژ</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.units.map((unit, index) => (
+                <tr>
+              <th scope="row">{index}</th>
+              <td>{unit.unitNumber}</td>
+              <td>{unit.owner}</td>
+              <td>{unit.resident}</td>
+              <td>{unit.area}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
 export default Units;
--- apartment/src/components/Units/unitsServices.js
+++ apartment/src/components/Units/unitsServices.js
@@ -1,1 +1,17 @@
 // ==== for fake data and API calls ==== 
+const fakeUnits = [{
+    id: 3,
+    unitNumber: 4,
+    owner: 43,
+    resident: 6,
+    area: 67
+},
+{
+    id: 6,
+    unitNumber: 6,
+    owner: 50,
+    resident: 7,
+    area: 80
+}]
+
+export const getData = () => fakeUnits
