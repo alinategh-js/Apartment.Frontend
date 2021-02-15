4ac12369db8fec7d03666589b51fb3c8f67290e5
Ali <a.nategh76@gmail.com>
2021-02-15 16:02:47 +0330

    nothing serious

--- apartment/src/components/Expenses/expenses.jsx
+++ apartment/src/components/Expenses/expenses.jsx
@@ -4,7 +4,40 @@ class Expenses extends Component {
     state = {  }
     render() { 
         return ( 
-            <h1>هزینه ها</h1>
+            <>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">عنوان</th>
+                        <th scope="col">نوع</th>
+                        <th scope="col">از</th>
+                        <th scope="col">تا</th>
+                        <th scope="col">مبلغ</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </>
          );
     }
 }
