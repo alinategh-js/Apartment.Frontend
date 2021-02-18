824cee609dd837f2ea9e5f867a54ba652855db3b
Farhad <farhadzohoorz@gmail.com>
2021-02-18 09:15:53 +0330

    Add unitId and isOwner columns to the table

--- apartment/src/components/People/people.jsx
+++ apartment/src/components/People/people.jsx
@@ -16,24 +16,30 @@ class People extends Component {
     return (
       <>
         <h1>آدم ها</h1>
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">نام</th>
-              <th scope="col">شماره تلفن</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.people.map((person, index) => (
+        <div className="row">
+          <table class="table">
+            <thead>
               <tr>
-                <th scope="row">{person.id}</th>
-                <td>{person.name}</td>
-                <td>{person.phone}</td>
+                <th scope="col">#</th>
+                <th scope="col">نام</th>
+                <th scope="col">شماره تلفن</th>
+                <th scope="col">شماره واحد</th>
+                <th scope="col">صاحب است؟</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {this.state.people.map((person, index) => (
+                <tr>
+                  <th scope="row">{person.id}</th>
+                  <td>{person.name}</td>
+                  <td>{person.phone}</td>
+                  <td>{person.unitId}</td>
+                  <td>{person.isOwner ? 1 : 0}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </>
     );
   }
--- apartment/src/components/People/peopleServices.js
+++ apartment/src/components/People/peopleServices.js
@@ -4,31 +4,43 @@ const fakeDatas = [
     id: 1,
     name: "ali",
     phone: "156156",
+    unitId: 2,
+    isOwner: false,
   },
   {
     id: 2,
     name: "farhad",
     phone: "454649",
+    unitId: 1,
+    isOwner: false,
   },
   {
     id: 3,
     name: "sahar",
     phone: "64346",
+    unitId: 4,
+    isOwner: true,
   },
   {
     id: 4,
     name: "zahra",
     phone: "14694",
+    unitId: 5,
+    isOwner: false,
   },
   {
     id: 5,
     name: "azin",
     phone: "189797",
+    unitId: 6,
+    isOwner: true,
   },
   {
     id: 6,
     name: "nazi",
     phone: "4894563",
+    unitId: 7,
+    isOwner: false,
   },
 ];
 
